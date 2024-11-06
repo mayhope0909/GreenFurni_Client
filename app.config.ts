e19ddc449c1cedb328ddce9a6b875cb85c2e4cca
Bichhop3589 <130751356+Bichhop3589@users.noreply.github.com>
2024-11-07 01:26:48 +0700

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -4,11 +4,19 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes'; // Đảm bảo đường dẫn chính xác đến app.routes.ts
 import { RouterModule } from '@angular/router';
 
+
+
+
+
+import { provideHttpClient } from '@angular/common/http';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideClientHydration(),
     importProvidersFrom(RouterModule),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
