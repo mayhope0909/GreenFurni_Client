9e53e121eebf069dacabc3dc844d9008d7b27e7c
Bichhop3589 <130751356+Bichhop3589@users.noreply.github.com>
2024-11-07 00:37:52 +0700

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -6,11 +6,15 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 import { PasswordauthenticationComponent } from './passwordauthentication/passwordauthentication.component';
 import { NewpasswordComponent } from './newpassword/newpassword.component';
+import { RouterOutlet } from '@angular/router';
+import { OrderManagementComponent } from "./order-management/order-management.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent, LoginComponent, HomepageComponent, ResetpasswordComponent, PasswordauthenticationComponent, NewpasswordComponent, RouterModule ],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent, HomepageComponent, ResetpasswordComponent, PasswordauthenticationComponent, NewpasswordComponent, RouterModule,OrderManagementComponent, ProductListComponent, ProductDetailComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
