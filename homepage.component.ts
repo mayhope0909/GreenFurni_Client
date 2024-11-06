f4a937601e75f05777a539f4a6484d31f5da407b
manman1210 <manhm22406@st.uel.edu.vn>
2024-11-06 23:37:44 +0700

    homepage

---
+++ homepage.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent {
+  faqQuestions = [
+    { question: 'Làm thế nào để gửi sản phẩm cần bảo hành cho GreenFurni?', answer: 'Liên hệ với bộ phận hỗ trợ để được hướng dẫn chi tiết.', showAnswer: false },
+    { question: 'Sản phẩm sau mua có được đổi trả không?', answer: 'Sản phẩm có thể đổi trả trong vòng 7 ngày.', showAnswer: false },
+    { question: 'Sản phẩm được bảo hành như thế nào?', answer: 'Chúng tôi cung cấp bảo hành 12 tháng cho tất cả sản phẩm.', showAnswer: false }
+  ];
+
+  toggleAnswer(index: number): void {
+    this.faqQuestions[index].showAnswer = !this.faqQuestions[index].showAnswer;
+  }
+}
