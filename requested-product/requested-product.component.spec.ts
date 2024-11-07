58e7f70fba538d25a45f7e877b28234d980c6325
mayhope0909 <130887709+mayhope0909@users.noreply.github.com>
2024-11-07 21:09:21 +0700

    Add files via upload

---
+++ requested-product/requested-product.component.spec.ts
@@ -0,0 +1,24 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { RequestedProductComponent } from "./RequestedProductComponent";
+
+// describe('RequestedProductComponent', () => {
+//   let component: RequestedProductComponent;
+//   let fixture: ComponentFixture<RequestedProductComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [RequestedProductComponent]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(RequestedProductComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
