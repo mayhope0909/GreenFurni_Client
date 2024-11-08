159b0219a146c84a63b0aee51ea7fa9fcfb502f7
Trantrunghieu2812 <151360766+Trantrunghieu2812@users.noreply.github.com>
2024-11-08 17:02:34 +0700

    Add files via upload

---
+++ chinhsach/chinhsach.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chinhsach',
+  standalone: true,
+  imports: [],
+  templateUrl: './chinhsach.component.html',
+  styleUrl: './chinhsach.component.css'
+})
+export class ChinhsachComponent {
+
+}
