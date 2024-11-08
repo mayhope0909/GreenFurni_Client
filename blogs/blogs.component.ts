0edc4e2ff2986753fc9bd37c20daedf01e76e6a9
Trantrunghieu2812 <151360766+Trantrunghieu2812@users.noreply.github.com>
2024-11-08 17:02:49 +0700

    Add files via upload

---
+++ blogs/blogs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-blogs',
+  templateUrl: './blogs.component.html',
+  styleUrls: ['./blogs.component.css'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class BlogsComponent {}
